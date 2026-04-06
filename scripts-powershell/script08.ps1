2d0dc6ade41671724b164d72bd5f9a1b46d5779a
Smith Braz <smithoffsec@gmail.com>
2026-04-06 18:49:48 -0300

    Create script08.ps1

---
+++ scripts-powershell/script08.ps1
@@ -0,0 +1,11 @@
+# Scripting Powershell
+param($p1)
+if(!$p1){
+    echo "RedScan Academy - Curso Red Team"
+    echo "5m1th - OffSec Team"
+    echo "Modo de uso: .\script.ps1 192.168.0"
+} else {
+foreach ($ip in 1..254) {
+echo "Disparando ping contra o target: $p1.$ip"
+}
+}
