04a2596a5b8853eea1a477c8763115c9f35a27dd
Smith Braz <smithoffsec@gmail.com>
2026-04-06 18:50:14 -0300

    Create script09.ps1

---
+++ scripts-powershell/script09.ps1
@@ -0,0 +1,11 @@
+# Scripting Powershell
+param($p1)
+if(!$p1){
+    echo "RedScan Academy - Curso Red Team"
+    echo "5m1th - OffSec Team"
+    echo "Modo de uso: .\script.ps1 192.168.0"
+} else {
+foreach ($ip in 1..254) {
+ping -n 1 "$p1.$ip" | Select-String "bytes=32"
+}
+}
