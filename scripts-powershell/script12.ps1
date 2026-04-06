5208fecdc0d0d69826e5b0ac5579fb126f4da9db
Smith Braz <smithoffsec@gmail.com>
2026-04-06 18:52:50 -0300

    Create script12.ps1

---
+++ scripts-powershell/script12.ps1
@@ -0,0 +1,14 @@
+# Scripting Powershell
+param($ip)
+if(!$ip){
+    echo "RedScan Academy - Curso Red Team"
+    echo "5m1th - OffSec Team"
+    echo "Modo de uso: .\script.ps1 192.168.0.1"
+} else {
+foreach ($porta in 1..1024){
+if (Test-NetConnection $ip -Port $porta -WarningAction SilentlyContinue -InformationLevel Quiet) {
+    echo "Port $porta Opened"
+} else {
+    echo "Port $porta Closed"
+}}
+}
